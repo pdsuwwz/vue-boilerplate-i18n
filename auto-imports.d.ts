94f32fb92908702c8ee029e7e6e0e7536ed2d0a8
Wisdom <pdsu.wwz@foxmail.com>
2024-02-04 14:39:26 +0800

    🧤 chore: upgrade latest deps (#269)

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -304,6 +304,7 @@ declare global {
 // for vue template auto import
 import { UnwrapRef } from 'vue'
 declare module 'vue' {
+  interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
     readonly _: UnwrapRef<typeof import('lodash-es')>
@@ -594,6 +595,7 @@ declare module 'vue' {
   }
 }
 declare module '@vue/runtime-core' {
+  interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
     readonly _: UnwrapRef<typeof import('lodash-es')>
