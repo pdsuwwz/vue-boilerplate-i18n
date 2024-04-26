03b40d3b36bd011cddb17842bb02778cf538d379
Wisdom <pdsu.wwz@foxmail.com>
2024-04-26 11:23:12 +0800

    🧑‍⚕️ fix: ignores for eslint

--- __tests__/IconFont.spec.ts
+++ __tests__/IconFont.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import IconFontComponent from '@/components/IconFont/index.vue'
